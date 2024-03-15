c0401bc41793368f95fc6286568d1bb7ef561f5b
Lucas Melo <lucas.melo044@gmail.com>
2024-03-15 20:49:02 -0300

    chore: add styled-components

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Home from './templates/App'
 
+import './styles/global-styles.css'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Home />
+       <Home />
   </React.StrictMode>,
 )
