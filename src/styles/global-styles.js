ab155948dadb712d4f3648f8edd2c1f3541a34c7
Lucas Melo <lucas.melo044@gmail.com>
2024-07-16 15:16:19 -0300

    styles: add new fonts family

--- src/styles/global-styles.js
+++ src/styles/global-styles.js
@@ -1,8 +1,23 @@
 import { createGlobalStyle, css } from 'styled-components'
 
 export const GlobalStyles = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,900;1,900&family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap');
+
   * {
     margin: 0;
     padding: 0;
+  }
+
+  html {
+    font-size: 62.5%;
+  }
+
+  body {
+    font-size: 1.4rem;
+    font-family: 'Open Sans', sans-serif;
+  }
+
+  h1 {
+    font-family: 'Montserrat', sans-serif;
   }
 `
